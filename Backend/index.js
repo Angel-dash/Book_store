77a581767ab5ec789a9143a48f3e0213c81b5e92
Angel-dash <njlghmr@gmail.com>
2023-10-27 17:24:45 +0545

    bookmodel added

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 const app = express();
@@ -6,6 +6,26 @@ const app = express();
 app.get("/", (request, response) => {
 	console.log(request);
 	return response.status(234).send("Welcome to the book store projects");
+});
+// route to add new book
+app.post("/books", async (resquest, response) => {
+	try {
+		if (
+			!resquest.body.title ||
+			!resquest.body.author ||
+			!request.body.publishYear
+		) {
+			return response.status(400).send({ message: "Send all required fileds" });
+		}
+		const newBook = {
+			title: request.body.title,
+			author: request.body.author,
+			publishYear: request.body.publishYear,
+		};
+	} catch (error) {
+		console.log(error);
+		response.status(500).send({ message: error.message });
+	}
 });
 
 mongoose
