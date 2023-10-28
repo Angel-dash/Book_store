3d643881416d8bf3106faa62aa0efd2a115f127c
Angel-dash <njlghmr@gmail.com>
2023-10-28 16:28:29 +0545

    middleware issue solved

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,21 @@
 import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 const app = express();
 
 app.get("/", (request, response) => {
 	console.log(request);
 	return response.status(234).send("Welcome to the book store projects");
 });
+
+app.use(express.json()); //Middleware for parsing request body
 // route to add new book
-app.post("/books", async (resquest, response) => {
+app.post("/books", async (request, response) => {
 	try {
 		if (
-			!resquest.body.title ||
-			!resquest.body.author ||
+			!request.body.title ||
+			!request.body.author ||
 			!request.body.publishYear
 		) {
 			return response.status(400).send({ message: "Send all required fileds" });
@@ -22,6 +25,8 @@ app.post("/books", async (resquest, response) => {
 			author: request.body.author,
 			publishYear: request.body.publishYear,
 		};
+		const book = await Book.create(newBook);
+		return response.status(201).send(book); //sending the content of the new book
 	} catch (error) {
 		console.log(error);
 		response.status(500).send({ message: error.message });
